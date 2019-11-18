d6cfdf3a3158e0058f0f1b44a8d5514b56dfd660
Shane <shane.baldauf@gmail.com>
2019-11-18 14:51:38 -0700

    test

--- pages/_.vue
+++ pages/_.vue
@@ -15,29 +15,29 @@
 </template>
 
 <script>
-  const loadData = function({lang, api, cacheVersion, errorCallback, version, path}) {
-    return api.get(`cdn/stories${path}`, {
-      version: version,
-      cv: cacheVersion,
-      resolve_links: 2
-    }).then(res => {
-      if(res.data) {
-        return res.data
-      }
-    }).catch(res => {
-      errorCallback({ statusCode: 404, message: 'Failed to receive content form api' })
-    })
-  }
+const loadData = function({lang, api, cacheVersion, errorCallback, version, path}) {
+  return api.get(`cdn/stories${path}`, {
+    version: version,
+    cv: cacheVersion,
+    resolve_links: 2
+  }).then(res => {
+    if(res.data) {
+      return res.data
+    }
+  }).catch(res => {
+    errorCallback({ statusCode: 404, message: 'Failed to receive content form api' })
+  })
+}
 
-  export default {
-    data () {
-      return { story: { content: {} } }
-    },
-    head () {
-      return {
-        title: this.story.content.metadata ? this.story.content.metadata.title : this.story.title,
-        titleTemplate: '%s | Function',
-        meta: [
+export default {
+  data () {
+    return { story: { content: {} } }
+  },
+  head () {
+    return {
+      title: this.story.content.metadata ? this.story.content.metadata.title : this.story.title,
+      titleTemplate: '%s | Function',
+      meta: [
         { hid: 'description', name: 'description', 
         content: this.story.content.metadata ? this.story.content.metadata.description : '' },
 
@@ -66,42 +66,49 @@
         content: this.story.content.ogDescription || 'Digitize your process' },
         { name: 'og:image',
         content: this.story.content.ogImage || 'http://a.storyblok.com/f/58905/2658x1348/a7dfc7be83/social-thumbnail.png' }
-        ]
-  }
-},
-mounted () {
-  this.$nextTick(() => {
-    this.$nuxt.$loading.start()
-    this.$nuxt.$loading.finish()
-  })
-
-  this.$storybridge.on(['input', 'published', 'change'], (event) => {
-    if (event.action == 'input') {
-      if (event.story.id === this.story.id) {
-        this.story.content = event.story.content
-      }
-    } else if (!event.slugChanged) {
-      window.location.reload()
-    }
-  })
-},
-asyncData (context) {
-  let editMode = false
-  if(context.payload) {
-    return { story: context.payload }
-  } else {
-    if (context.query._storyblok || context.isDev || (typeof window !== 'undefined' && window.localStorage.getItem('_storyblok_draft_mode'))) {
-      if (typeof window !== 'undefined') {
-        window.localStorage.setItem('_storyblok_draft_mode', '1')
-        if (window.location == window.parent.location) {
-          window.localStorage.removeItem('_storyblok_draft_mode')
+      ],
+      script: [
+        { 
+          innerHTML: 'window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag(\'js\', new Date()); gtag(\'config\', \'UA-55889031-1\'); gtag(\'config\', \'AW-699780384\');', 
+          type: 'text/javascript',
+          charset: 'utf-8'
         }
+      ]
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      this.$nuxt.$loading.start()
+      this.$nuxt.$loading.finish()
+    })
+
+    this.$storybridge.on(['input', 'published', 'change'], (event) => {
+      if (event.action == 'input') {
+        if (event.story.id === this.story.id) {
+          this.story.content = event.story.content
+        }
+      } else if (!event.slugChanged) {
+        window.location.reload()
+      }
+    })
+  },
+  asyncData (context) {
+    let editMode = false
+    if(context.payload) {
+      return { story: context.payload }
+    } else {
+      if (context.query._storyblok || context.isDev || (typeof window !== 'undefined' && window.localStorage.getItem('_storyblok_draft_mode'))) {
+        if (typeof window !== 'undefined') {
+          window.localStorage.setItem('_storyblok_draft_mode', '1')
+          if (window.location == window.parent.location) {
+            window.localStorage.removeItem('_storyblok_draft_mode')
+          }
+        }
+
+        editMode = true
       }
 
-      editMode = true
-    }
-
-    let version = editMode ? 'draft' : 'published'
+      let version = editMode ? 'draft' : 'published'
 
       // this wants to be lang agnostic
       let path = context.route.path === '/' ? '/home' : context.route.path === '/zh-cn/' ? '/zh-cn/home' : context.route.path
