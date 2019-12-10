f80fb2cd227744c8d746f1ddba02b92ee1cc543e
Shane Baldauf <shane.baldauf@gmail.com>
2019-12-10 11:04:47 -0700

    story id

--- nuxt.config.js
+++ nuxt.config.js
@@ -103,7 +103,9 @@ const config = {
       let page = 1
       let routes = ['/']
 
-      console.log(process.env.INCOMING_HOOK_BODY, 'dat')
+      let thing = JSON.parse(process.env.INCOMING_HOOK_BODY)
+
+      console.log(thing ? thing.story_id, 'dat')
 
       // Load space and receive latest cache version key to improve performance
       axios.get(`https://api.storyblok.com/v1/cdn/spaces/me?token=${blokToken}`).then((space_res) => {
